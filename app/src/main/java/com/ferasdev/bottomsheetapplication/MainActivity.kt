5b633077eb7cf4040b39c7ce0950cb335219745b
feras alnowiser <ferasdev@gmail.com>
2023-04-17 00:04:32 +0300

    add: the toolbar on MainActivity page

--- app/src/main/java/com/ferasdev/bottomsheetapplication/MainActivity.kt
+++ app/src/main/java/com/ferasdev/bottomsheetapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ferasdev.bottomsheetapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ferasdev.bottomsheetapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        // R.layout.activity_main
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbarActivityMain)
     }
 }

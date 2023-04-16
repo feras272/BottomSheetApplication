6d6e49acd61f01413c1873c6652e54bfc4d57f24
feras alnowiser <ferasdev@gmail.com>
2023-04-17 00:07:21 +0300

    add: ScaleItem.kt data class

---
+++ app/src/main/java/com/ferasdev/bottomsheetapplication/ScaleItem.kt
@@ -0,0 +1,8 @@
+package com.ferasdev.bottomsheetapplication
+
+data class ScaleItem(
+    var titleProperty:String,
+    var imageProperty: Int,
+    var contentProperty:String,
+    var propertyVisibility:Boolean = false
+)
